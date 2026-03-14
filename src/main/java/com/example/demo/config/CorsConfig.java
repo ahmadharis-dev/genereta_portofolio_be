c7d3c9a9374a78af2779888fd0e5ccd8d99496ac
ahmadharis-dev <Ahmaharis6@gmail.com>
2026-03-15 03:50:17 +0700

    #Add Config Cors

--- src/main/java/com/example/demo/config/CorsConfig.java
+++ src/main/java/com/example/demo/config/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8082") // alamat frontend Vue
+                .allowedOrigins("http://localhost:4200","https://fe-portofolio.serversandgen.web.id") // alamat frontend Vue
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
